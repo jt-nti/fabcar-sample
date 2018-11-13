605e40251a15673bad2d0a75fe6ae089769d09b3
James Taylor <jamest@uk.ibm.com>
2018-11-13 15:59:04 +0000

    Add missing contract index.js file
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

---
+++ contracts/fabcar/index.js
@@ -0,0 +1,9 @@
+/*
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+*/
+'use strict';
+
+const FabcarContract = require('./fabcar')
+module.exports.contracts = [FabcarContract];
