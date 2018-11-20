68413cc48f1a07ba119cbd78bd22d96d05b4b94b
James Taylor <jamest@uk.ibm.com>
2018-11-20 15:33:26 +0000

    Replace old fabcar js contract with new ts version
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- contracts/fabcar/index.js
+++
@@ -1,9 +0,0 @@
-/*
-# Copyright IBM Corp. All Rights Reserved.
-#
-# SPDX-License-Identifier: Apache-2.0
-*/
-'use strict';
-
-const FabcarContract = require('./fabcar')
-module.exports.contracts = [FabcarContract];
